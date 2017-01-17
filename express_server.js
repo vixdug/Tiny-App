d7f7aee088216c4460740e62bd655b03f22d15df
vixdug <victoria.duggan@shopify.com>
2017-01-17 23:26:34 +0000

    added .post for the url creation

--- express_server.js
+++ express_server.js
@@ -6,13 +6,24 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs")
 
+
+function generateRandomString() {
+var result = Math.random().toString(36).substr(2, 6)
+return result
+}
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+
 };
 
 app.get("/", (req, res) => {
   res.end("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -26,12 +37,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.post("/urls/create", (req, res) => {
+  let longURL = req.body["longURL"];
+  let shortURL = generateRandomString();
+  console.log(longURL);
+  console.log(shortURL);
+
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
